bfa8e11d80bab488c1b4046afc5416f17b445078
kamihejaz <83613913+kamihejaz@users.noreply.github.com>
2024-08-26 17:02:48 +0000

    VALIDATE BAD DOG

--- validate.sh
+++ validate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 cue vet src/tokens/solana.tokenlist.json automerge/schema.cue -d '#Tokenlist'
+BAD DOG, BAD DOG,G8jJDisvUSu7iYpe7m9YTE9uyDYvpuVskVX7gi9Ssmry,9,https://pbs.twimg.com/profile_images/1824460870937645056/p0vOzveG_400x400.jpg,TRUE
